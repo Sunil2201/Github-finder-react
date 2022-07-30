a402c292bd157b75f24370e2ff852f61283a63cd
Sunil Ballani <ballanisunil123@gmail.com>
2022-07-30 17:11:31 +0530

    Changed width

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -14,7 +14,7 @@ function RepoItem({repo}) {
 
     return( 
         <div className='mb-2 rounded-md card bg-gray-800 hover:bg-gray-900 '>
-            <div className="card-body">
+            <div className="card-body w-108">
                 <h3 className="mb-2 text-xl font-semibold">
                     <a href={html_url}>
                         <FaLink className='inline mr-1'/> {name}
